561447c9261de118199700a3e273d82dc54ffb20
Brandon Li <sirbrandonthenerd@gmail.com>
2024-09-27 22:10:51 -0400

    grid background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,5 +7,10 @@
 	<meta name="" content="" />
 </svelte:head>
 
+<main class="w-full h-screen flex">
+	<section class="w-96 z-10">section 1</section>
+	<section class="flex-1 z-10">section 2</section>
+</main>
+
 <style>
 </style>
