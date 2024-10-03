cce09ca22c604ac80fbf8b2d4d46c941cedb641f
Brandon Li <sirbrandonthenerd@gmail.com>
2024-10-03 14:25:15 -0400

    tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,8 +14,16 @@
 	<section class="z-10 max-lg:h-[44rem] lg:w-[32rem] flex items-center justify-center relative">
 		<Header />
 	</section>
-	<section class="flex-1 z-10">section 2</section>
+	<section class="flex-1 z-10 flex flex-col justify-center items-center px-12 xl:px-24">
+		<div id="item">
+			<div></div>
+		</div>
+	</section>
 </main>
 
-<style>
+<style lang="postcss">
+	#item {
+		@apply max-w-2xl w-full bg-stone-200/40 p-4 rounded-md;
+		backdrop-filter: blur(5px);
+	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import Window from './Window.svelte';
-	
+
 	import folio_h from '$lib/folio-h.png?enhanced';
-	
+
 	import biohazard from '$lib/post-its/biohazard.png?enhanced';
 	import dish from '$lib/post-its/dish.png?enhanced';
 	import greek from '$lib/post-its/greek.png?enhanced';
@@ -13,20 +13,16 @@
 	<span class="fixed">
 		<div class="pseudo-window absolute {classes}" id="post-it">
 			<div class="w-[13rem] h-[13rem]">
-				<enhanced:img class="w-full h-full" src={url} alt="post-it" {id} />
+				<enhanced:img class="w-full h-full" src={url} alt="post-it-{id}" {id} />
 			</div>
 		</div>
 	</span>
 {/snippet}
 
 <div>
-	{@render post_it(rocket, 'bottom-[15rem] left-[6rem]', 'rocket')}
-	<enhanced:img
-		src={folio_h}
-		class="max-w-[32rem] drop-shadow-lg"
-		alt="Header"
-	/>
-	{@render post_it(biohazard, 'bottom-[27rem] left-[17rem]', 'biohazard')}
+	<enhanced:img src={folio_h} class="max-w-[32rem] drop-shadow-lg" alt="Header" />
+	{@render post_it(rocket, 'bottom-[16rem] left-[6rem]', 'rocket')}
+	{@render post_it(biohazard, 'bottom-[14rem] left-[17rem]', 'biohazard')}
 	<span class="fixed">
 		<Window class="absolute w-[19rem] bottom-[20.5rem] left-[1rem]" title="Intro1">
 			<h5 class="mb-2">Welcome!</h5>
@@ -39,12 +35,12 @@
 			<div class="divider"></div>
 			<div class="flex flex-row-reverse gap-2">
 				<button>Cancel</button>
-				<button>Okay</button>
+				<button>OK</button>
 			</div>
 		</Window>
 	</span>
-	{@render post_it(dish, 'bottom-[3rem] left-[4rem]', 'dish')}
-	{@render post_it(greek, 'bottom-[1rem] left-[15rem]', 'greek')}
+	{@render post_it(dish, 'top-[-3rem] left-[4rem]', 'dish')}
+	{@render post_it(greek, 'top-[-1rem] left-[15rem]', 'greek')}
 </div>
 
 <style lang="postcss">
@@ -53,33 +49,36 @@
 		background: linear-gradient(0deg, hsl(var(--hue) 60% 90%), hsl(var(--hue) 60% 85%));
 		border-color: hsl(var(--hue), 30%, 60%);
 		animation-name: float;
-		animation-duration: 12s;
+		animation-duration: var(--l);
 		animation-timing-function: ease-in-out;
 		animation-iteration-count: infinite;
 		animation-delay: var(--d);
 		animation-direction: var(--dir);
 		transform: translate3d(0);
-		
 	}
 
 	span.fixed :has(#rocket) {
 		--hue: 70;
 		--d: -5s;
+		--l: 11s;
 	}
 
 	span.fixed :has(#biohazard) {
 		--hue: 300;
-		--d: -10s;
+		--d: -11s;
+		--l: 12s;
 	}
 
 	span.fixed :has(#dish) {
 		--hue: 50;
-		--d: -6s;
+		--d: 0s;
+		--l: 12s;
 	}
 
 	span.fixed :has(#greek) {
 		--hue: 200;
-		--d: -11s;
+		--d: -9s;
+		--l: 17s;
 	}
 
 	@keyframes float {
