801d61a7630ff3f3b89a5b288ea0f49e93a9facd
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2026-01-13 18:52:47 -0500

    add screenshot

--- src/routes/Synthesthesia/Synthestesia.svelte
+++ src/routes/Synthesthesia/Synthestesia.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+    import screenshot from "./screenshot.png?enhanced";
+</script>
+
 <span class="relative lg:mt-[max(calc(50vh-19rem),2rem)]">
+    <enhanced:img src={screenshot} class="w-96 m-auto rotate-3"></enhanced:img>
 	<div
-		class="pseudo-window !w-max max-w-80 !h-max max-h-80 absolute right-1 prose-sm p-2 rotate-2 z-50"
+		class="pseudo-window !w-max max-w-80 !h-max max-h-80 absolute right-2 -mt-16 prose-sm p-2 rotate-3 z-50"
 		id="post-it"
 		style:--hue={20}
 	>
@@ -14,7 +19,7 @@
 			themselves.
 		</p>
 	</div>
-	<div class="mt-32 pseudo-window">
+	<div class="mt-16 pseudo-window">
 		<iframe
 		    class="w-full aspect-video"
 			src="https://www.youtube.com/embed/KmtkdJ5LhUE?si=iYkQuSzp1vPEyd9k"
